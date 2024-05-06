087098507571286b1706414d4a1d8720647c0e7a
Dave Borgeest <david.borgeest@gmail.com>
2024-05-06 12:40:50 +1000

    Getting the song search working

--- lib/widgets/song_list.dart
+++ lib/widgets/song_list.dart
@@ -6,24 +6,30 @@ import 'package:provider/provider.dart';
 
 import '../services/svc_songs.dart';
 
+// Here's our SongList, the rockstar of our app!
 class SongList extends StatefulWidget {
   SongList({super.key});
 
+  // Our trusty song service, always ready to fetch us some tunes
   final SongsService songsService = SongsService();
 
   @override
-  // ignore: library_private_types_in_public_api
   _SongListState createState() => _SongListState();
 }
 
 class _SongListState extends State<SongList> {
   var logger = Logger();
 
+  // A future that holds all our songs. It's like a time capsule for music!
   Future<List<Song>> allSongsFuture = getAllSongs();
 
+  TextEditingController _searchController = TextEditingController();
+  List<Song> _filteredSongs = [];
+  List<Song> _allSongs = [];
+
+  // A function that gets all songs. It's like a musical treasure hunt!
   static Future<List<Song>> getAllSongs() {
     Logger().d('Getting all songs.');
-    // ignore: no_leading_underscores_for_local_identifiers
     Future<List<Song>> _allSongs = SongsService().allSongs;
 
     return _allSongs;
@@ -32,17 +38,20 @@ class _SongListState extends State<SongList> {
   @override
   build(BuildContext context) {
     logger.d('Building the SongList widget.');
+    // Our main stage, where all the action happens
     return Expanded(
       child: Container(
+        // Some padding to give our widget room to breathe
         margin: const EdgeInsets.only(right: 6.0, left: 6.0, top: 6.0),
+        // A little decoration never hurt anyone
         decoration: BoxDecoration(
-          color: Colors.white, // Your desired color
-          borderRadius: BorderRadius.circular(9.0),
+          color: Colors.white, // As pure as a new guitar string
+          borderRadius: BorderRadius.circular(9.0), // Smooth as a jazz solo
           border: Border.all(
             color: const Color.fromRGBO(
-                203, 203, 203, 1.0), // Your desired border color
-            width: 1.0, // Your desired border width
-          ), // Your desired corner radius
+                203, 203, 203, 1.0), // A border as solid as a drum beat
+            width: 1.0, // Just the right thickness
+          ),
         ),
         child: Column(children: <Widget>[
           Row(
@@ -57,30 +66,31 @@ class _SongListState extends State<SongList> {
                   ),
                 ),
               ),
+              // A button that does... something. It's a mystery!
               IconButton(
                 icon: const Icon(Icons.sort),
                 onPressed: () {
-                  // Handle button press
+                  logger.d("Sort songs button pressed.");
                 },
               ),
               IconButton(
                 icon: const Icon(Icons.add),
                 onPressed: () {
                   // Handle button press
+                  logger.d("Add song button pressed.");
                 },
               )
             ],
           ),
 
-          //========================
-          // Search Box
-
+          // Our search box, the key to finding your favorite tunes
           Padding(
             padding: const EdgeInsets.all(2.0),
             child: Container(
               height: 45.0,
               alignment: Alignment.center,
               child: TextField(
+                controller: _searchController,
                 textAlign: TextAlign.left,
                 textAlignVertical: TextAlignVertical.center,
                 decoration: InputDecoration(
@@ -88,7 +98,10 @@ class _SongListState extends State<SongList> {
                   hintText: "Search",
                   prefixIcon: const Icon(Icons.search),
                   suffixIcon: IconButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      logger.d("Clear search button pressed.");
+                      _searchController.clear();
+                    },
                     icon: const Icon(Icons.clear),
                   ),
                   border: const OutlineInputBorder(
@@ -99,19 +112,21 @@ class _SongListState extends State<SongList> {
             ),
           ),
 
-          //========================
-          // Song List
-
+          // The heart of our app, the song list
           Expanded(
             child: FutureBuilder<List<Song>>(
                 future: allSongsFuture,
                 builder: (context, snapshot) {
                   if (snapshot.connectionState == ConnectionState.waiting) {
+                    logger.d("Waiting for songs.");
                     return const CircularProgressIndicator();
                   } else if (snapshot.hasData) {
-                    final posts = snapshot.data!;
-                    return buildSongs(posts);
+                    logger.d("Got songs.");
+                    _allSongs = snapshot.data!;
+                    _filteredSongs = _allSongs;
+                    return buildSongs(_filteredSongs);
                   } else {
+                    logger.d("No songs available.");
                     return const Text("No data available");
                   }
                 }),
@@ -125,15 +140,17 @@ class _SongListState extends State<SongList> {
     return _SongListState();
   }
 
-  Widget buildSongs(List<Song> songs) {
+  // A function to build our song list. It's like a concert promoter for our app!
+  Widget buildSongs(List<Song> filteredSongs) {
+    logger.d("Building the song list with ${filteredSongs.length} songs.");
+
     return ListView.builder(
-      itemCount: songs.length,
+      itemCount: filteredSongs.length,
       itemBuilder: (context, index) {
-        final thisSong = songs[index];
+        final thisSong = filteredSongs[index];
         return Container(
           margin: const EdgeInsets.symmetric(vertical: 1, horizontal: 10),
           padding: const EdgeInsets.symmetric(vertical: 1, horizontal: 5),
-          // height: 100,
           width: double.maxFinite,
           child: Row(
             children: [
@@ -141,40 +158,35 @@ class _SongListState extends State<SongList> {
                 flex: 3,
                 child: Consumer<CurrentSongProvider>(
                     builder: (context, currentSongProvider, child) {
-                  return Container(
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                        //primary: Colors.black,
-                        backgroundColor:
-                            currentSongProvider.currentSong.title ==
-                                    thisSong.title
-                                ? Color.fromARGB(255, 237, 243, 248)
-                                : Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
+                  return TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: currentSongProvider.currentSong.title ==
+                              thisSong.title
+                          ? const Color.fromARGB(255, 237, 243, 248)
+                          : Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      onPressed: () {
-                        logger.d('Song selected: ${thisSong.title}');
-                        currentSongProvider.setCurrentSong(thisSong);
-                      },
-                      child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                thisSong.title,
-                                style: const TextStyle(
-                                  fontSize: 16, // Set your desired font size
-                                  fontWeight: FontWeight
-                                      .bold, // Set your desired font weight
-                                ),
-                              ),
-                              Text(thisSong.artist),
-                            ],
-                          )),
                     ),
+                    onPressed: () {
+                      logger.d('Song selected: ${thisSong.title}');
+                      currentSongProvider.setCurrentSong(thisSong);
+                    },
+                    child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              thisSong.title,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(thisSong.artist),
+                          ],
+                        )),
                   );
                 }),
               ),
@@ -183,5 +195,38 @@ class _SongListState extends State<SongList> {
         );
       },
     );
+  }
+
+  _onSearchChanged() {
+    logger.d("Search text: ${_searchController.text}");
+    if (_searchController.text.isEmpty || _searchController.text.length < 3) {
+      logger.d("No search phrase - _allSongs: $_allSongs");
+      setState(() {
+        _filteredSongs = _allSongs;
+      });
+    } else {
+      logger.d("Filtering songs - ${_searchController.text}");
+      setState(() {
+        _filteredSongs = _allSongs
+            .where((song) => song.title
+                .toLowerCase()
+                .contains(_searchController.text.toLowerCase()))
+            .toList();
+      });
+      logger.d("Filtered songs: ${_filteredSongs.length}");
+    }
+  }
+
+  @override
+  void dispose() {
+    _searchController.removeListener(_onSearchChanged);
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(_onSearchChanged);
   }
 }
